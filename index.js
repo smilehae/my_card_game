abf288a301496e2bd4e77017ac8475fe34afc030
smilehae <mihaepyun@gmail.com>
2021-11-09 18:29:01 +0900

    win, lose draw 구현 완료

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const cardContainer = document.querySelector("#card-container");
 function shuffleCard(n) {
   //랜덤한, 0~n 까지의 숫자 사이에서 두개를 골라서, 그 숫자를 보수와 위치를 바꿔준다.
   let shuffleCount = cards.length;
-  while (shuffleCount * 2) {
+  while (shuffleCount * 3) {
     //0~n/2 중에서 랜덤한 인덱스 2개 고르기
     const chooseIndex1 = Math.round(Math.random() * Math.floor(n - 1));
     const chooseIndex2 = Math.round(Math.random() * Math.floor(n - 1));
@@ -67,7 +67,7 @@ function shuffleCard(n) {
 //n개의 카드 생성
 function generateCard(n) {
   //0~n까지의 숫자를 카드에 집어넣기.
-  for (let i = 0; i < n / 2; i++) {
+  for (let i = 0; i < n; i++) {
     const card = {
       index: i,
       name: animals[i],
@@ -76,7 +76,7 @@ function generateCard(n) {
     card.number = i;
     cards.push(card);
     const newCard = JSON.parse(JSON.stringify(card));
-    newCard.number = i + n / 2;
+    newCard.number = i + n;
     cards.push(newCard);
   }
   shuffleCard(n);
@@ -88,10 +88,20 @@ function generateCard(n) {
     .join("");
   cardContainer.innerHTML = cardContent;
 }
-generateCard(10);
+const numberOfCard = 6;
+generateCard(numberOfCard);
 const displayCards = Array.from(document.querySelectorAll(".card"));
+const trialsText = document.querySelector(".trials");
+const players = document.querySelectorAll(".player");
+const scoreText1 = players[0].querySelector(".score");
+const scoreText2 = players[1].querySelector(".score");
+
 let choosenCard = -1;
 let firstCardIndex = -1;
+let trials = 0;
+let player1Score = 0;
+let player2Score = 0;
+let ansCount = 0;
 
 displayCards.forEach((card) =>
   card.addEventListener("click", () => {
@@ -105,25 +115,63 @@ displayCards.forEach((card) =>
     cardPic.style.color = `${colors[animalIndex]}`;
 
     //점수 획득용 장치
+    //첫번쨰 카드 선택
     if (choosenCard == -1) {
       choosenCard = animalIndex;
       firstCardIndex = numberIndex;
-    } else {
+    }
+    //두번째 카드 선택
+    else {
       if (choosenCard == animalIndex) {
         console.log("correct!");
         choosenCard = -1;
+        ansCount++;
+        if (trials == 0 || trials % 2 == 0) {
+          player1Score += 10;
+          scoreText1.textContent = player1Score;
+        } else {
+          player2Score += 10;
+          scoreText2.textContent = player2Score;
+        }
+        //여기서 우승자 발생!
+        if (ansCount == numberOfCard) {
+          if (player1Score == player2Score) {
+            players[0].classList.add("draw");
+            players[1].classList.add("draw");
+            scoreText1.textContent = "draw🎈";
+            scoreText2.textContent = "draw🎈";
+          } else if (player1Score > player2Score) {
+            console.log("player1 wins!");
+            players[0].classList.add("winner");
+            players[1].classList.add("loser");
+            scoreText1.textContent = "win🥇";
+            scoreText2.textContent = "lose💧";
+          } else {
+            console.log("player2 wins!");
+            players[0].classList.add("loser");
+            players[1].classList.add("winner");
+            scoreText2.textContent = "win🥇";
+            scoreText1.textContent = "lose💧";
+          }
+        }
       } else {
         choosenCard = -1;
         console.log("wrong!");
+        const pickedCard = cardContainer.querySelector(
+          `.card[data-number="${firstCardIndex}"]`
+        );
         setTimeout(() => {
           pickedCard.classList.add("closed");
           card.classList.add("closed");
         }, 1000);
       }
-
-      const pickedCard = cardContainer.querySelector(
-        `.card[data-number="${firstCardIndex}"]`
-      );
+      trials++;
+      trialsText.textContent = trials;
+      setTimeout(() => {
+        players.forEach((player) => {
+          player.classList.toggle("active");
+        });
+      }, 1000);
     }
   })
 );
